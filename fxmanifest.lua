81b908b0ada120f46a1972427d5ac0a12a2722b7
bishwo9898 <64731632+bishwo9898@users.noreply.github.com>
2023-01-03 18:54:17 +0545

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'oatmeal'
+description 'Moneywash Script By oatmeal'
+version '1.0.0'
+
+shared_script {
+    '@qb-core/shared.lua'
+}
+
+server_script 'server/*.lua'
+client_script 'client/*.lua'
+
+lua54 'yes'
